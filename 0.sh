3b19c9de018ca72728a38dda2c3ff2b8a7d8e60d
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2015-06-23 04:35:40 +0000

    EC2 doesnt have make

--- 0.sh
+++ 0.sh
@@ -1,6 +1,6 @@
 # wget http://bit.ly/nishiosetup; bash nishiosetup することで最初に諸々必要なものをインストールする
 set -x
-sudo apt-get install -y git-core
+sudo apt-get install -y git-core make
 cd
 git clone https://github.com/nishio/setup.git
 cd setup
